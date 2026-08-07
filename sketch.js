35afb39c23a4bf9508a90d5810e99e21662d2f92
Sooraj44882 <sooraj44882@gmail.com>
2026-08-07 20:09:01 +0530

    added undo and redo logic

--- sketch.js
+++ sketch.js
@@ -1,15 +1,15 @@
 let currentTool = 'pencil';
-let pencilBtn;
-let eraserBtn;
-let sizeDropdown;
-let colorPicker;
-let clearBtn;
-let saveBtn;
+let pencilBtn ,eraserBtn ,sizeDropdown ,colorPicker ,undoBtn ,redoBtn;
+let clearBtn ,saveBtn;
+
+let isDrawing=false;
+
+let undoStack=[];
+let redoStack=[];
 
 function setup() {
   let canvas = createCanvas(800, 600);
   canvas.parent('canvas-container');
-
   background(255);
 
   pencilBtn = document.getElementById('pencilBtn');
@@ -17,15 +17,40 @@ function setup() {
   sizeDropdown = document.getElementById('sizeDropdown');
   colorPicker = document.getElementById('colorPicker');
   clearBtn = document.getElementById("clearBtn");
-  saveBtn = document.getElementById("saveBtn")
+  saveBtn = document.getElementById("saveBtn");
+  undoBtn = document.getElementById("undoBtn");
+  redoBtn = document.getElementById("redoBtn");
+
+  
+  saveState();
+
+  undoBtn.addEventListener("click",function(){
+    if(undoStack.length>1){
+      let currentState= undoStack.pop();
+      redoStack.push(currentState);
+
+      let previousState=undoStack[undoStack.length-1];
+      image(previousState,0,0);
+    }
+  });
+
+  redoBtn.addEventListener("click",function(){
+    if(redoStack.length>0){
+      let nextState=redoStack.pop();
+      undoStack.push(nextState);
+      image(nextState,0,0);
+    }
+  });
+
 
   saveBtn.addEventListener('click',function(){
     saveCanvas("mypaint",'png');
-  })
+  });
 
   clearBtn.addEventListener('click',function(){
     background(255);
-  })
+    saveState();
+  });
 
   pencilBtn.addEventListener('click', function() {
     currentTool = 'pencil';
@@ -40,8 +65,14 @@ function setup() {
   });
 }
 
+function mousePressed() {
+  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+    isDrawing = true;
+  }
+}
+
 function draw() {
-  if (mouseIsPressed === true) {
+  if (isDrawing === true) {
     
     let brushSize = sizeDropdown.value;
     strokeWeight(brushSize);
@@ -53,5 +84,23 @@ function draw() {
     }
     
     line(pmouseX, pmouseY, mouseX, mouseY);
+  }
+}
+
+
+function mouseReleased(){
+  if (isDrawing=== true) {
+  saveState();
+  redoStack=[];
+  isDrawing=false;
+  console.log(undoStack)
+  }
+}
+
+function saveState(){
+  undoStack.push(get());
+
+  if (undoStack.length > 10) {
+    undoStack.shift(); 
   }
 }
